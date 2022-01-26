3c878d64c40a1bad2107ce4fc7779c40699c5ca2
Lolli Di <290romchik@gmail.com>
2022-01-26 17:16:43 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public static class Model
+    {
+        public static List<string> listOperations = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static int selectedOperation = 0;
+        public static string answer;
+    }
+}
---
+++ MVVM/ViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        public TextBox TBoxNum1;
+        public TextBox TBoxNum2;
+
+        public List<string> CBData
+        {
+            get => Model.listOperations;
+        }
+        public int SelectIndexCB
+        {
+            set
+            {
+                Model.selectedOperation = value;
+                onPropChan("SelectedOperation");
+            }
+        }
+        public string SelectedOperation
+        {
+            get
+            {
+                switch (Model.selectedOperation)
+                {
+                    case 0:
+                        return "+";
+                    case 1:
+                        return "-";
+                    case 2:
+                        return "х";
+                    default:
+                        return "/";
+                }
+            }
+        }
+
+        public string Answer
+        {
+            get => Model.answer;
+            set
+            {
+                Model.answer = value;
+                onPropChan("Answer");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void onPropChan([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+        
+        public RoutedCommand BtnCommand { get; set; } = new RoutedCommand();
+        public CommandBinding CBinding1;
+        public void CommandAnswer(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                double firstNum = Convert.ToDouble(TBoxNum1.Text);
+                double secondNum = Convert.ToDouble(TBoxNum2.Text);
+                if (Double.IsNaN(firstNum) || Double.IsNaN(secondNum))
+                {
+                    throw new Exception("Введите цифры для выполнения операций!");
+                }
+                switch (Model.selectedOperation)
+                {
+                    case 0:
+                        Answer = "" + (firstNum + secondNum);
+                        break;
+                    case 1:
+                        Answer = "" + (firstNum - secondNum);
+                        break;
+                    case 2:
+                        Answer = "" + (firstNum * secondNum);
+                        break;
+                    case 3:
+                        Answer = "" + (firstNum / secondNum);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        public ViewModel()
+        {
+            CBinding1 = new CommandBinding(BtnCommand);
+            CBinding1.Executed += CommandAnswer;
+        }
+
+    }
+}
